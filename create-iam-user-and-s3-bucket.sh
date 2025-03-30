6e2444aea9b8b9d880b40fd93c94c86d71b33617
Oguzhan Yilmaz <oguzhan@hepapi.com>
2025-03-31 00:55:05 +0300

    update shell script

--- create-iam-user-and-s3-bucket.sh
+++ create-iam-user-and-s3-bucket.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# PREFIX="${PREFIX:-argocd-backup-s3}"
+# Set your company prefix
 PREFIX="mycompany-argocd-backup-s3"
 
-# Get AWS Account ID
+# Get AWS Account Info
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION=$(aws configure get region 2>/dev/null || echo "eu-west-1")
 
 echo "AWS_ACCOUNT_ID: ${AWS_ACCOUNT_ID}"
 echo "AWS_REGION: ${AWS_REGION}"
-
 
 # Create bucket name using AWS Account ID as suffix
 BUCKET_NAME="${PREFIX}-${AWS_ACCOUNT_ID}"
@@ -19,16 +18,12 @@ echo "BUCKET_NAME: ${BUCKET_NAME}"
 echo "IAM_USER_NAME: ${IAM_USER_NAME}"
 
 # Create S3 Bucket
-echo "Creating S3 Bucket: s3://${BUCKET_NAME}"
 aws s3 mb "s3://${BUCKET_NAME}" --region "${AWS_REGION}"
 
-# Create IAM User
-echo "Creating IAM User: s3://${IAM_USER_NAME}"
+# Create IAM User and Policy
 aws iam create-user --user-name "${IAM_USER_NAME}"
 
-# Create Restrictive IAM Policy
 POLICY_NAME="${IAM_USER_NAME}-bucket-access-policy"
-echo "Creating IAM Policy: ${POLICY_NAME}"
 aws iam create-policy \
     --policy-name "${POLICY_NAME}" \
     --policy-document '{
@@ -51,7 +46,6 @@ aws iam create-policy \
 }'
 
 # Attach Policy to User
-echo "Attach the IAM Policy to User"
 aws iam attach-user-policy \
     --user-name "${IAM_USER_NAME}" \
     --policy-arn "$(aws iam list-policies --query "Policies[?PolicyName=='${POLICY_NAME}'].Arn" --output text)"
@@ -59,12 +53,16 @@ aws iam attach-user-policy \
 # Create Access Keys
 CREDENTIALS=$(aws iam create-access-key --user-name "${IAM_USER_NAME}")
 
-# Print Credentials and Information
+# Print Helm Values
 echo "------ SUCCESS ------"
-
-echo "Bucket Name: ${BUCKET_NAME}"
-echo "IAM User: ${IAM_USER_NAME}"
-echo "AWS Region: ${AWS_REGION}"
-echo "AWS Account ID: ${AWS_ACCOUNT_ID}"
-echo "Access Key ID: $(echo "${CREDENTIALS}" | jq -r '.AccessKey.AccessKeyId')"
-echo "Secret Access Key: $(echo "${CREDENTIALS}" | jq -r '.AccessKey.SecretAccessKey')"
+echo "Helm values.yaml:"
+echo ""
+echo "secretEnvVars:"
+echo "  AWS_ACCESS_KEY_ID: '$(echo "${CREDENTIALS}" | jq -r '.AccessKey.AccessKeyId')'"
+echo "  AWS_SECRET_ACCESS_KEY: '$(echo "${CREDENTIALS}" | jq -r '.AccessKey.SecretAccessKey')'"
+echo "  AWS_DEFAULT_REGION: ${AWS_REGION}"
+echo "  S3_BUCKET_NAME: ${BUCKET_NAME}"
+echo "  S3_UPLOAD_PREFIX: my-argo-instance/"
+echo "  ARGOCD_SERVER: argocd-server.argocd"
+echo "  ARGOCD_ADMIN_PASSWORD: ''"
+echo "  AWS_S3_ENDPOINT: 'https://s3.amazonaws.com'"
