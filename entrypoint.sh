675dde7b824bfd71ac53a39d2a70e88ca9b87ca9
Oguzhan Yilmaz <oguzhan@hepapi.com>
2025-04-30 21:33:42 +0300

    fix entrypoint script info msg

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ echo "------------*------------*------------"
 # ----------- LOGIN TO ARGOCD -----------
 echo "Logging in to ArgoCD Server: ${ARGOCD_SERVER}"
 if [ -z "${ARGOCD_ADMIN_USERNAME}" ];then
-  echo "WARNING: ARGOCD_ADMIN_USERNAME is empty; continue with 'admin' user"
+  echo "ARGOCD_ADMIN_USERNAME env var is empty; defaulting to 'admin' user..."
   export ARGOCD_ADMIN_USERNAME="admin"
 fi
 
